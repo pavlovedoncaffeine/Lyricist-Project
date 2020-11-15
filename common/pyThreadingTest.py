c2bc80309c64ae289cca38b47a1c06f7954abce9
pavlovedoncaffeine <akash.ravichandran@gmail.com>
2020-11-15 18:34:44 -0500

    Additional testing and experimentation, returned to functional script state (default: should not request authorization and uses an implicit grant from the web player/desktop app, using the WebAPI to determine the currently playing track. This should suffice for all Windows/linux/macOS overlay application purposes. The Android and iOS Spotify SDKs should help me recreate the same functionality for those platforms with relative ease now that I've covered my basics.

--- common/pyThreadingTest.py
+++ common/pyThreadingTest.py
@@ -1,4 +1,6 @@
-# multithreading practice python script - pavlovedoncaffeine (for Spotify Lyricist Project)
+# this is only really for Windows/Linux/MacOS overlay applications as spotify provides an iOS and Android SDK that doesn't require the Web API to get metadata for the player's state.
+# multithreading oAuth2.0 python test script - pavlovedoncaffeine (for Spotify Lyricist Project)
+
 import time
 import sys
 import threading
@@ -12,7 +14,11 @@ from spotipy.oauth2 import *
 from six.moves.BaseHTTPServer import HTTPServer
 
 #serverThreadExitCode = 0
-lyScope = 'user-read-playback-state'
+#lyScope = 'user-read-currently-playing'    #Doesn't allow access to what the user's listening to in private mode, and still requires authorization. 
+                                            # Could use some additional scope testing to see if any of them allow us to read the current track even in private mode
+#lyScope = None  #If the user chooses not to share Authorize Spotify to share data with Lyricist, we can attempt to progress with just the implicitly available information when the user is *not* in private-mode
+
+lyScope = None  # || (if user consider's authorization..) lyScope = 'user-read-currently-playing user-read-playback-position playlist-read-private'
 SPOTIFY_CLIENT_ID = '7b45f66cbae54e518a9fb2e5717e6e6d'
 SPOTIFY_CLIENT_SECRET = '108a6846a16f47b5ba15ef306775396c'
 SPOTIFY_REDIRECT = "http://localhost:8888/callback/"
@@ -53,16 +59,16 @@ def main():
         oAuthServer.server_close()
         print (time.asctime() + " - Lyricist: Spotify OAuth2.0 Server shutting down.\n")
     
-    #to-do: see if the Client_secret can be omitted from the SpotifyOAuth initialization in the below statement. If yes, then we ought to be in business and can be erased from the git repo
-    spLyricist = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=SPOTIFY_CLIENT_ID, client_secret=SPOTIFY_CLIENT_SECRET, redirect_uri=SPOTIFY_REDIRECT, state=None, scope=lyScope, username='dk_krypton', show_dialog=True))
-    query = spLyricist.currently_playing()
+    
+    spLyricist = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=SPOTIFY_CLIENT_ID, client_secret=SPOTIFY_CLIENT_SECRET, redirect_uri=SPOTIFY_REDIRECT, state=None, scope=lyScope, username='dk_krypton', show_dialog=False))
+    query = spLyricist.currently_playing()      #query = spLyricist.user_playlist_is_following(playlist_id='7xB6mybJCHX92r3ZPS1FJ2', user_ids=['bluecan309'])
+    
 
     if spLyricist is not None and query is not None:
         print (json.dumps(query, sort_keys=True, indent=4))
-        exit(43)
     else:
         print("Could not retrieve current playback information...")
-        exit(1300135)
+    exit(200)
 
 if __name__ == '__main__':
    main()
