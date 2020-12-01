cb32e4607311f3251898548797022476381a7544
pavlovedoncaffeine <akash.ravichandran@gmail.com>
2020-12-01 18:01:07 -0500

    Added in SQL based functions to store information per track to the lyricist database with the track information and the lyric's file stored on the server's file system.

---
+++ common/lyricistSQL.py
@@ -0,0 +1,90 @@
+import os
+import traceback
+#from datetime import date, datetime
+import mysql.connector
+from mysql.connector import errorcode
+
+# track_lyric JSON structure
+# { 'track_name' : <String/varchar>,
+#   'artists'    : <String/varchar>,
+#   'is_explicit': <Boolean>,
+#   'duration_ms': <int>,
+#   'spotify_trackID': <String/varchar>,
+#   'apple_trackID': <String/varchar>,
+#   'bpm': <int>,
+#   'is_cover'   : <Boolean>,
+#   'has_lyrics' : <Boolean>,
+#   'courtesy_of': <String/varchar>,
+#   'lyric_file' : <String/varchar>
+# }
+
+
+def select_file(track_name=None, artists=None):
+    return
+
+
+class lyricistDB:
+
+    def __init__(self):
+        self.lySQL = None
+        self.cursor = None
+
+        try:
+            self.lySQL = mysql.connector.connect(
+                user='pavlovedoncaffeine', password='C0mput3r!', host='127.0.0.1', port=3306, database='LyricistDB')
+            self.cursor = lySQL.cursor()
+        except mysql.connector.Error as err:
+            if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+                print("Error authorizing access to the Lyricist database.")
+            elif err.errno == errorcode.ER_BAD_DB_ERROR:
+                print("Lyricist: Database not reachable.")
+            print(traceback.format_exc()+"\n")
+
+    def updateTrack(self, track=None):
+        if track is None:
+            return False
+        try:
+            updateRow = ("UPDATE lyricist_tracks SET album = %s, is_explicit = %s, duration_ms = %s, spotify_trackID = %s, apple_trackID = %s, bpm = %s, is_cover = %s, has_lyrics = %s, courtesy_of = %s, lyric_file = %s "
+                         "WHERE track_name = %s AND artists = %s")
+            self.cursor.execute(updateRow, track)
+            assert(commitToDB())
+            return True
+        except:
+            print(traceback.format_exc())
+            return False
+
+    def insertTrackFromDict(self, track=None):
+        if track is None:
+            return False
+        try:
+            insertRow = ("INSERT INTO lyricist_tracks "
+                         "(track_name, artists, album, is_explicit, duration_ms, spotify_trackID, apple_trackID, bpm, is_cover, has_lyrics, courtesy_of, lyric_file) "
+                         "VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)")
+            self.cursor.execute(insertRow, track)
+            assert(commitToDB())
+            return True
+        except:
+            print(traceback.format_exc())
+            return False
+
+    def insertTrackDetails(self, track_name, artists, album=None, is_explicit=False, duration_ms=None, spotify_trackID=None, apple_trackID=None, is_cover=False, has_lyrics=False, courtesy_of=False, lyric_file=None):
+        try:
+            insertRow = ("INSERT INTO lyricist_tracks "
+                         "(track_name, artists, album, is_explicit, duration_ms, spotify_trackID, apple_trackID, bpm, is_cover, has_lyrics, courtesy_of, lyric_file) "
+                         "VALUES (%(track_name)s, %(artists)s, %(album)s, %(is_explicit)s, %(duration_ms)s, %(spotify_trackID)s, %(apple_trackID)s, %(bpm)s, %(is_cover)s, %(has_lyrics)s, %(courtesy_of)s, %(lyric_file)s)")
+            data = (track_name, artists, album, is_explicit, duration_ms, spotify_trackID,
+                    apple_trackID, is_cover, has_lyrics, courtesy_of, lyric_file)
+            self.cursor.execute(insertRow, data)
+            assert(commitToDB())
+            return True
+        except:
+            print(traceback.format_exc())
+            return False
+
+    def commitToDB(self):
+        try:
+            self.lySQL.commit()
+            return True
+        except:
+            print(traceback.format_exc())
+            return False
--- common/pyAuthServer.py
+++ common/pyAuthServer.py
@@ -2,21 +2,27 @@
 # multithreading oAuth2.0 python test script - pavlovedoncaffeine (for Spotify Lyricist Project)
 
 import os
-import sys, time, threading
-import six, requests, json
+import sys
+import time
+import threading
+import six
+import requests
+import json
 import traceback
+import pathlib
 import spotipy
-import azlyrics
 
-from pyLogging import *
+from azlyrics import songs, artists
+from common.pyLogging import *
+from common.lyricistSQL import *
 from spotipy.oauth2 import SpotifyOAuth
 from spotipy.oauth2 import *
 from six.moves.BaseHTTPServer import HTTPServer
 
-#serverThreadExitCode = 0
-#lyScope = 'user-read-currently-playing'    #Doesn't allow access to what the user's listening to in private mode, and still requires authorization. 
-                                            # Could use some additional scope testing to see if any of them allow us to read the current track even in private mode
-#lyScope = None  #If the user chooses not to share Authorize Spotify to share data with Lyricist, we can attempt to progress with just the implicitly available information when the user is *not* in private-mode
+#  serverThreadExitCode = 0
+# lyScope = 'user-read-currently-playing'    #Doesn't allow access to what the user's listening to in private mode, and still requires authorization.
+# Could use some additional scope testing to see if any of them allow us to read the current track even in private mode
+# lyScope = None  #If the user chooses not to share Authorize Spotify to share data with Lyricist, we can attempt to progress with just the implicitly available information when the user is *not* in private-mode
 
 lyScope = 'user-read-currently-playing user-read-playback-position playlist-read-private'
 SPOTIFY_REDIRECT = "http://localhost:8888/callback/"
@@ -34,14 +40,17 @@ with open(secrets, 'r') as secretsFile:
                 SPOTIFY_CLIENT_ID = words[1].strip()
             elif words[0] == 'SPOTIFY_CLIENT_SECRET':
                 SPOTIFY_CLIENT_SECRET = words[1].strip()
-    
+
 LOGFILE = os.path.join(os.getcwd(), "common", "logs", genLogfileName())
-DATA_FOLDER = "E:\\Projects\\Lyricist\\Data\\"
+DATA_FOLDER = "E:\\Projects\\Lyricist\\Data"
+
+lyricistDB = lyricistDB()
+
 
 class localServerThread (threading.Thread):
     def __init__(self, server=None, name='Spotify OAuth2.0 Authentication Server'):
         threading.Thread.__init__(self)
-        print ('Initializing...')
+        print('Initializing...')
         self.server = server
         self.name = name
 
@@ -53,78 +62,123 @@ def getTracksFromPlaylist(spLyr=None, plistID=None):
     queryResult = None
     index = 0
     total = 0
-    
+
     try:
         assert(spLyr is not None)
         assert(plistID is not None)
-    except exc:
-        raise AssertionError("Invalid argument(s) to retrieve track list. Aborting operation.")
+    except:
+        raise AssertionError(
+            "Invalid argument(s) to retrieve track list. Aborting operation.")
         return None
-
     queryResult = spLyr.playlist(plistID, fields='tracks(total)')
     total = queryResult['tracks']['total']
 
-    trackList = []    
+    trackList = []
     while index < total:
-        queryResult = spLyr.playlist_tracks(plistID, fields='items(track(name, artists(name),id,explicit,duration_ms))', offset=index)['items']
+        queryResult = spLyr.playlist_tracks(
+            plistID, fields='items(track(name, artists(name),id,explicit,duration_ms))', offset=index)['items']
         index += 100
         for val in queryResult:
-            song = val["track"]
             track = {}
+            song = val["track"]
             track["name"] = song["name"]
-            track["artists"] = ', '.join([artist["name"] for artist in song["artists"]])
+            track["artists"] = ", ".join(
+                [artist["name"] for artist in song["artists"]])
+            track['album'] = song["album"]
             track["spotify_ID"] = song["id"]
             track["explicit"] = song["explicit"]
             track["duration_ms"] = song["duration_ms"]
             trackList.append(track)
 
-    #print(json.dumps(trackList, indent=4))         # debugging
+    # print(json.dumps(trackList, indent=4))         # debugging
     return trackList
 
 
-
-# To-do: November 21st, 2020 implement a rudimentary system to pull lyrics and write to file in the Data Folder.s
-# def getLyrics(song=None, artist=None):
-# return None    
+# To-do: implement a rudimentary system to pull lyrics and write to file in the Data Folder.s
+def saveLyrics(song=None, artists=None):
+    if song is None or artists is None:
+        return None
+    artist = artists.split(", ")[0]
+    lyricsAZ = azlyrics.lyrics(song, artist)
+    if lyricsAZ is not None:  # if the lyrics are found, save it to a folder in "E:\Data\"
+        artist_dir = os.path.join(DATA_FOLDER, artist)
+        pathlib.Path(artist_dir).mkdir(parents=True, exist_ok=True)
+        lyricsFilePath = os.path.join(artist_dir, song) + ".ly"
+        try:
+            with open(lyricsFilePath, 'x') as lyricsFile:
+                lyricsFile.write(song + " - " + artists + "\n")
+                for line in lyricsAZ:
+                    lyricsFile.write(line)
+                lyricsFile.write("***")  # EOF indicator for lyricist?
+            print("Lyricist: Lyrics for:\n\t" + song + " - " + artists +
+                  "\nhave been saved in the following directory: " + artist_dir + "\n")
+        except FileExistsError as exc:
+            print("Lyricist: Lyrics for:\n\t" + song + " - " + artists +
+                  "\nalready exist in the following data folder: " + artist_dir + "\n")
+            print(traceback.format_exc())
+            return False
+        return True
+    else:
+        return False  # debugging... still-to-implement: search other databases such as musixmatch and genius etc etc to generate lyrics files
 
 
 def main():
-    
+
     with writeLog(open(LOGFILE, 'a')):
-        
-        oAuthServer = spotipy.oauth2.start_local_http_server(port=8888)     # Spotify OAuth2.0 to authorize the web API token systems
+
+        # Spotify OAuth2.0 to authorize the web API token systems
+        oAuthServer = spotipy.oauth2.start_local_http_server(port=8888)
         oAuthThread = localServerThread(oAuthServer)
-        
+
         try:
             try:
                 if oAuthThread is not None:
-                    print(genTimestamp() + 'Lyricist: Launching Spotify oAuth2 Authorization Service.')
+                    print(
+                        genTimestamp() + 'Lyricist: Launching Spotify OAuth2.0 Authorization Service.')
                     oAuthThread.start()
             except Exception as exc:
-                print(genTimestamp() + 'Lyricist: Spotify oAuth2 Authorization Service could not be initialized.')
-                traceback.format()
+                print(genTimestamp() +
+                      'Lyricist: Spotify OAuth2.0 Authorization Service could not be initialized.')
+                print(traceback.format_exc())
 
         except KeyboardInterrupt:
             oAuthThread.join(1)
             oAuthServer.shutdown()
             oAuthServer.server_close()
-            raise PermissionError(genTimestamp() + 'Lyricist: Spotify OAuth2.0 Server Initialization Aborted.')
-            traceback.format()
-            
+            raise PermissionError(genTimestamp() +
+                                  'Lyricist: Spotify OAuth2.0 Server Initialization Aborted.')
+            print(traceback.format_exc())
+
         finally:
             oAuthThread.join(1)
             oAuthServer.shutdown()
             oAuthServer.server_close()
-            print (genTimestamp() + 'Lyricist: Spotify oAuth2 Server shutting down')
-        
-        spLyricist = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=SPOTIFY_CLIENT_ID, client_secret=SPOTIFY_CLIENT_SECRET, redirect_uri=SPOTIFY_REDIRECT, state=None, scope=lyScope, username='dk_krypton', show_dialog=False))
-        
+            print(genTimestamp() + 'Lyricist: Spotify OAuth2.0 Server shutting down')
+
+        spLyricist = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=SPOTIFY_CLIENT_ID, client_secret=SPOTIFY_CLIENT_SECRET,
+                                                               redirect_uri=SPOTIFY_REDIRECT, state=None, scope=lyScope, username='dk_krypton', show_dialog=False))
+
+        test_playlist = '6jviAN7MIgh36bLHEPI4DL'  # bring the house down : dk_krypton
         if spLyricist is not None:
-            queryResult = getTracksFromPlaylist(spLyricist, '6jviAN7MIgh36bLHEPI4DL')
-            print(json.dumps(queryResult, indent=4))
-            exit()
+            playlistTracks = getTracksFromPlaylist(spLyricist, test_playlist)
+
+            # 1. for-each through list of tracks in playlist
+            # 2. check if each track in playlist exists in database, and if it has lyrics
+            # 3. if yes, return lyricFile from db's storage
+            # 4. otherwise, run saveLyrics(song, artists)
+            for track in playlistTracks:
+                # 1. SELECT track, artists from lyricist_tracks (lyricistDB.selectRow(self, track_name, artists)) # to be implemented
+                # (2.) + (3.) from above ^^
+
+                # (4)
+                result = saveLyrics(track['track_name'], track['artists'])
+
+                # print(json.dumps(queryResult, indent=4))          # debugging
+                # exit()                                            # debugging
         else:
-            raise PermissionError(genTimestamp() + 'Lyricist: Could not retrieve playlist.\nPlease ensure that Spotify is not in \'Private Mode\'')
+            raise PermissionError(genTimestamp() +
+                                  'Lyricist: Could not retrieve playlist.\nPlease ensure that Spotify is not in \'Private Mode\'')
+
 
 if __name__ == '__main__':
-   main()
+    main()
